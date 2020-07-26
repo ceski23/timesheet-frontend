8e2c6b66c953827df765d6a30f4bc5eeef38375c
Cezary Bober <ceski23@gmail.com>
2020-07-26 15:55:21 +0200

    🏷️ Fix types usage in redirecting

--- src/components/auth/RedirectAfterLogin.tsx
+++ src/components/auth/RedirectAfterLogin.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { useLocation, Redirect } from 'react-router';
 import { ROUTE_HOME } from 'routes';
 
+interface LocationState {
+  nextLocation?: string;
+}
+
 export const RedirectAfterLogin = () => {
-  const location = useLocation();
+  const location = useLocation<LocationState>();
 
   return (
-    <Redirect to={(location.state as any).nextLocation || ROUTE_HOME} />
+    <Redirect to={location.state.nextLocation || ROUTE_HOME} />
   );
 };
